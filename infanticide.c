5d77a3c1cfda7bbf341c66b617c148f3befc9021
aidris823 <pokeanand@gmail.com>
2018-11-16 11:02:34 -0500

    Baby's first meaningful commit!

--- infanticide.c
+++ infanticide.c
@@ -3,6 +3,22 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+int process(){
+  int child_one = fork();
+  int child_two = fork();
+  //Child Instructions
+  if (child_one){
+    printf("%d",getpid());
+    sleep(5,20);
+    printf("Hello, I am child one.  I am now ded.");
+  }
+  if (child_two){
+    printf("%d",getpid());
+    sleep(5,20);
+    printf("Greetings, child two is I.  Soon, child two WAS I.");
+  }
+  //Parent Instructions
+}
 int main(){
   return 0;
 }
