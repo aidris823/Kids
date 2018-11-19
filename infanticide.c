504e94a880d2185afe1574c9227bacee71a29e1b
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-11-19 02:41:57 -0500

    Baby's first done assignemnt?

--- infanticide.c
+++ infanticide.c
@@ -4,21 +4,28 @@
 #include <unistd.h>
 
 int process(){
-  int child_one = fork();
+  int child = fork();
   int child_two = fork();
+  printf("Initial message: Why was Pavlov's hair so soft?  Classical conditioning.");
   //Child Instructions
   if (child_one){
-    printf("%d",getpid());
-    sleep(5,20);
-    printf("Hello, I am child one.  I am now ded.");
+    printf("Child PID: %d",getpid());
+    sleep(rand() % 15 + 5 );//range: 5 - 20
+    printf("Hello, I am child one.  I am now AWAKE.");
   }
   if (child_two){
-    printf("%d",getpid());
-    sleep(5,20);
-    printf("Greetings, child two is I.  Soon, child two WAS I.");
+    printf("Child PID: %d",getpid());
+    sleep(rand() % 15 + 5);
+    printf("Greetings, child two is I.  I am 2woke4u.");
   }
   //Parent Instructions
+  int x;
+  //Find child's PID
+  int child_pid = wait(&x);
+  printf("Child of PID %d slept for %d seconds.",child_pid,WEXITSTATUS(x));
+  return 0;
 }
 int main(){
+  process();
   return 0;
 }
